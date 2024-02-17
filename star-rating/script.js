a1a96c70558db1b3d2c5059115f644346b787688
mithun20522 <mithun20522@iiitd.ac.in>
2024-02-17 23:16:50 +0530

    added color in stars based on the diffrent conditions.

--- star-rating/script.js
+++ star-rating/script.js
@@ -1,9 +1,29 @@
 const stars = document.querySelectorAll('.star');
 const rating = document.querySelector('.rating');
 
+let curr = 0;
 stars.forEach((elm) => {
     elm.addEventListener('click', (e) =>{
         rating.textContent = `You Rated ${e.target.dataset.value} stars.`;
+        colortheStars(e.target.dataset.value);
+    })
+
+    elm.addEventListener('mouseover', (e) => {
+        colortheStars(e.target.dataset.value);
+    })
+
+    elm.addEventListener('mouseout', (e) => {
+        colortheStars(0);
     })
 
 })
+
+function colortheStars(value){
+    stars.forEach(star => star.classList.remove('highlight'));
+
+    stars.forEach((star, idx) => {
+        if(idx < value){
+            star.classList.add('highlight');
+        }
+    })
+}
