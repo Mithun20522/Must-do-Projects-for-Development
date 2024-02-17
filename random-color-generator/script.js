13769f00fb434eedb71f662ed9487b3648eb4c7b
mithun20522 <mithun20522@iiitd.ac.in>
2024-02-17 19:48:49 +0530

    rgb part is also done.

--- random-color-generator/script.js
+++ random-color-generator/script.js
@@ -1,6 +1,6 @@
 const hexvalue = document.querySelector('.generated-hex-color');
 const hexBtn = document.querySelector('.hex-btn');
-const main = document.querySelector('.left');
+const left = document.querySelector('.left');
 hexBtn.addEventListener('click', () => {
     const hexcolor = "0123456789ABCDEF";
     let color = "#";
@@ -10,6 +10,24 @@ hexBtn.addEventListener('click', () => {
     }
     
     hexvalue.textContent = color;
-    main.style.backgroundColor = color;
+    left.style.backgroundColor = color;
 
+})
+
+const rgbvalue = document.querySelector('.generated-rgb-color');
+const rgbBtn = document.querySelector('.rgb-btn');
+const right = document.querySelector('.right');
+
+const redVal = document.querySelector('#red');
+const greenVal = document.querySelector('#green');
+const blueVal = document.querySelector('#blue');
+
+rgbBtn.addEventListener('click', () => {
+    const red = redVal.value;
+    const green = greenVal.value;
+    const blue = blueVal.value;
+    const rgbColor = `rgb(${red},${green},${blue})`;
+
+    rgbvalue.textContent = rgbColor;
+    right.style.backgroundColor = rgbColor;
 })
