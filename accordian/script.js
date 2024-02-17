9eb99be45cbe60cb0762206cd9e47a61c8723c93
mithun20522 <mithun20522@iiitd.ac.in>
2024-02-17 17:20:15 +0530

    multi tab open completed.

--- accordian/script.js
+++ accordian/script.js
@@ -2,24 +2,30 @@ const accordianContent = document.querySelectorAll(".accordian-content");
 
 const len = accordianContent.length;
 
-for(let i=0;i<len;i++){
-    const question = accordianContent[i].querySelector('.question');
-    const answer = accordianContent[i].querySelector('.answer');
-    const show = accordianContent[i].querySelector('.show');
-
-    show.addEventListener("click", () => {
-        answer.classList.toggle('show-answer');
-        if(answer.classList.contains('show-answer')){
-            answer.style.display = "block";
-            show.textContent = "-";
-        }
-        else{
-            answer.style.display = "none";
-            show.textContent = "+";
-        }
-    })
-
-    answer.style.display = "none";
-    show.textContent = "+";
-
+function multiOpen(accordianContent, len){
+    for(let i=0;i<len;i++){
+        const question = accordianContent[i].querySelector('.question');
+        const answer = accordianContent[i].querySelector('.answer');
+        const show = accordianContent[i].querySelector('.show');
+    
+        show.addEventListener("click", () => {
+            answer.classList.toggle('show-answer');
+            if(answer.classList.contains('show-answer')){
+                answer.style.display = "block";
+                show.textContent = "-";
+            }
+            else{
+                answer.style.display = "none";
+                show.textContent = "+";
+            }
+        })
+        answer.style.display = "none";
+        show.textContent = "+";
+    
+    }
 }
+
+multiOpen(accordianContent, len);
+
+
+
